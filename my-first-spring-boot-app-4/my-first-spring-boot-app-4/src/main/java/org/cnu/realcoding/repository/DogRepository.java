e6de2bf5fc35d72f344e5be6d17507f5f4cf9ae7
zezeon <jjaewon527@gmail.com>
2021-04-07 02:25:43 +0900

    API 추가By201602050

--- my-first-spring-boot-app-4/my-first-spring-boot-app-4/src/main/java/org/cnu/realcoding/repository/DogRepository.java
+++ my-first-spring-boot-app-4/my-first-spring-boot-app-4/src/main/java/org/cnu/realcoding/repository/DogRepository.java
@@ -15,10 +15,26 @@ public class DogRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public Dog findDog(String name) {
-       return mongoTemplate
+    public Dog findDogByName(String name) {
+        return mongoTemplate
                 .findOne(
                         Query.query(Criteria.where("name").is(name)),
+                        Dog.class // T -> generic 여기서는 Dog 객체
+                );
+    }
+
+    public Dog findDogByOwnerName(String ownerName) {
+        return mongoTemplate
+                .findOne(
+                        Query.query(Criteria.where("ownerName").is(ownerName)),
+                        Dog.class // T -> generic 여기서는 Dog 객체
+                );
+    }
+
+    public Dog findDogByOwnerPhoneNumber(String ownerPhoneNumber) {
+        return mongoTemplate
+                .findOne(
+                        Query.query(Criteria.where("ownerPhoneNumber").is(ownerPhoneNumber)),
                         Dog.class // T -> generic 여기서는 Dog 객체
                 );
     }
@@ -30,4 +46,14 @@ public class DogRepository {
     public List<Dog> findAllDog() {
         return mongoTemplate.findAll(Dog.class);
     }
+
+    /*public Dog findDogByAll(String name, String ownerName, String ownerPhoneNumber) {
+        return mongoTemplate
+                .findOne(
+                        Query.query(Criteria.where("name").is(name)
+                                .andOperator(Criteria.where("ownerName").is(ownerName)
+                                        .andOperator(Criteria.where("ownerPhoneNumber").is(ownerPhoneNumber)))),
+                        Dog.class // T -> generic 여기서는 Dog 객체
+                );
+    }*/
 }
