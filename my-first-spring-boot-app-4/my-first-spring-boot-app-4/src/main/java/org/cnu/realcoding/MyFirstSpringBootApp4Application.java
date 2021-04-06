18808de3e93881a318c975dbe5b3b9f3a4e828ed
Seonghan Kim, Dudu <80890062+Dudu-CNU@users.noreply.github.com>
2021-04-06 20:11:45 +0900

    basement of spring-boot

---
+++ my-first-spring-boot-app-4/my-first-spring-boot-app-4/src/main/java/org/cnu/realcoding/MyFirstSpringBootApp4Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp4Application.class, args);
+	}
+
+}
